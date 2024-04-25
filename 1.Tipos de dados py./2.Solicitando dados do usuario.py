3221b0771009058de696a60488e8e6b4216d26d7
Juliocesarsenai <159816707+Juliocesarsenai@users.noreply.github.com>
2024-04-25 11:19:33 -0300

    Delete 1.Tipos de dados py. directory

--- 1.Tipos de dados py./2.Solicitando dados do usuario.py
+++
@@ -1,6 +0,0 @@
-nome=input("Digite o seu nome:")
-idade=int(input("Digite a sua idade:"))
-altura=float(input("Digite a sua altura:"))
-print(f"Nome: {nome}")
-print(f"Idade:{idade}")
-print(f"Altura:{altura}")
